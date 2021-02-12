09d1dd28bf1fe8b690445de340fd3cb2034cc17c
aterrell4 <37730288+aterrell4@users.noreply.github.com>
2021-02-11 20:54:59 -0500

    Update FixQBDat.ps1

--- FixQBDat.ps1
+++ FixQBDat.ps1
@@ -2,14 +2,15 @@
 #created: 2-11-2021
 #written by: Adam Terrell 
 #contact: aterrell@vertisys.com
-$QBRegistrationOriginal = Get-Content -path "C:\temp\qbregistration.dat" -raw 
+$DATPath = "C:\temp\qbregistration.dat"
+$QBRegistrationOriginal = Get-Content -path $DATPath -raw 
 $QBFind = @("pro","superpro","bel","belacct")
 $QBReplace = "accountant"
 foreach ($QBFindVal in $QBFind) {
     if($QBRegistrationOriginal -match "\b$QBFindVal\b") {
         write-host -ForegroundColor Green "Found Instance of $QBFindVal To Replace"
 $QBRegistrationReplaced = $QBRegistrationOriginal -replace "\b$QBFindVal\b", $QBReplace
-Set-Content -Path "C:\temp\qbregistration.dat" -Value $QBRegistrationReplaced
+Set-Content -Path $DATPath -Value $QBRegistrationReplaced
     }
     else {
         write-host -ForegroundColor Green "No Instances of $QBFindVal Found"
